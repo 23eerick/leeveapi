271dcc872bdb89dd75a03b12a276e4b63840a184
23eerick <erickwelberth1@hotmail.com>
2023-01-06 09:14:53 -0300

    connect db

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+import dot from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(express.json())
+dot.config().parsed
 
+const db = process.env.DATABASE
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Inicio da aplicação')
 })
 
+mongoose.set('strictQuery', true)
+
+const initDatabase = async () => {
+  await mongoose.connect(db)
+    .then(() => {
+      console.log('connected database')
+    })
+    .catch((err) => {
+      console.log(`database not connected, ${err}`)
+    })
+}
+
 app.listen(port, () => {
   console.log(`API Rest for leeve front started at ${port} 🚀`)
 });
 
 
+const initApp = async () => {
+  await initDatabase()
+}
+
+initApp()
